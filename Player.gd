4d6a2f6715408e06c0cdaf736f8a7409410df4e5
Raymond Fu <raymondspam29@gmail.com>
2023-10-11 22:12:21 -0400

    Revert "4way wasd movemnt"
    This reverts commit 2557fd42d19c88e03e7a6fcfa7686df9827ffd03.

--- Player.gd
+++ Player.gd
@@ -1,20 +1,16 @@
 extends KinematicBody2D
 
 var speed = 200
+var target = null
 var velocity = Vector2.ZERO
 
-func get_input():
-	if Input.is_action_pressed("topDownForward"):
-		velocity.y -= 1
-	elif Input.is_action_pressed("topDownBackwards"):
-		velocity.y += 1
-	elif Input.is_action_pressed("topDownRight"):
-		velocity.x += 1
-	elif Input.is_action_pressed("topDownLeft"):
-		velocity.x -= 1
-	velocity = velocity.normalized() * speed
+func _input(event):
+	if event.is_action_pressed('click'):
+		target = event.position
 
 func _physics_process(delta):
-	velocity = Vector2.ZERO
-	get_input()
-	velocity = move_and_slide(velocity)
+	if target:
+		velocity = (target - position).normalized() * speed
+
+		if position.distance_to(target) > 5:
+			velocity = move_and_slide(velocity)
