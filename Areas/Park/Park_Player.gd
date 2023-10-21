e2accbe52e19fe5e1bdb3775ae65912e2772a2a5
stxrrymuffin <jessica.chen270412@gmail.com>
2023-10-21 02:16:37 -0400

    add MW art animation and edit player

--- Areas/Park/Park_Player.gd
+++ Areas/Park/Park_Player.gd
@@ -5,16 +5,35 @@ var velocity = Vector2.ZERO
 var score = 0
 var recycleHand = 0
 var wasteHand = 0
+var direction = 1
 
 func get_input():
 	if Input.is_action_pressed("topDownForward"):
 		velocity.y -= 1
+		direction = 1
+		$AnimatedSprite.play("upWalk")
 	elif Input.is_action_pressed("topDownBackwards"):
 		velocity.y += 1
+		direction = 2
+		$AnimatedSprite.play("downWalk")
 	elif Input.is_action_pressed("topDownRight"):
 		velocity.x += 1
+		direction = 3
+		$AnimatedSprite.play("rightWalk")
 	elif Input.is_action_pressed("topDownLeft"):
 		velocity.x -= 1
+		direction = 4
+		$AnimatedSprite.play("leftWalk")
+	else:
+			velocity = Vector2.ZERO
+			if(direction == 1):
+				$AnimatedSprite.play("upIdle")
+			if(direction == 2):
+				$AnimatedSprite.play("downIdle")
+			if(direction == 3):
+				$AnimatedSprite.play("rightIdle")
+			if(direction == 4):
+				$AnimatedSprite.play("leftIdle")
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
