0df6db1c5258277f928c6c3309220d9cf97ee7d5
Raymond Fu <raymondspam29@gmail.com>
2023-11-22 18:09:36 -0500

    main menu and fix dialogue

--- DialogManager.gd
+++ DialogManager.gd
@@ -34,7 +34,7 @@ func _show_text_box():
 	text_box = text_box_scene.instance()
 	text_box.connect("finished_displaying", self, "_on_text_box_finished_displaying")
 	print(get_tree().root.get_children())
-	get_tree().root.get_children()[1].get_node("CanvasLayer").add_child(text_box)
+	get_tree().root.get_children()[-1].get_node("CanvasLayer").add_child(text_box)
 	#get_tree().root.add_child(text_box)
 	text_box.set_global_position(text_box_position)
 	
---
+++ UI/MainMenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Areas/MainWorld.tscn")
+
+
+func _on_Options_pressed():
+	pass # Replace with function body.
