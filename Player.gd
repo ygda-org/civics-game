9644be41b3f700b91610b5424dd8b86b459add25
Mihtosis <miihto26@gmail.com>
2023-11-21 21:18:37 -0500

    adding sprinting (x) and additional interact button (z)
    hold x to sprint
    z is now an additional interact button
    :))

--- Player.gd
+++ Player.gd
@@ -6,6 +6,13 @@ var direction = 1
 var frozen = false
 
 func get_input():
+	var sprint_multiplier = 1.0
+	$AnimatedSprite.speed_scale = sprint_multiplier
+
+	if Input.is_action_pressed("sprint"):
+		sprint_multiplier = 2.0
+		$AnimatedSprite.speed_scale = sprint_multiplier
+
 	if Input.is_action_pressed("topDownForward") or Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 		$AnimatedSprite.play("walkUp")
@@ -18,21 +25,22 @@ func get_input():
 		velocity.x += 1
 		$AnimatedSprite.play("walkRight")
 		direction = 3
-	elif Input.is_action_pressed("topDownLeft")or Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("topDownLeft") or Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 		$AnimatedSprite.play("walkLeft")
 		direction = 4
 	else:
-			velocity = Vector2.ZERO
-			if(direction == 1):
-				$AnimatedSprite.play("idleUp")
-			if(direction == 2):
-				$AnimatedSprite.play("idleDown")
-			if(direction == 3):
-				$AnimatedSprite.play("idleRight")
-			if(direction == 4):
-				$AnimatedSprite.play("idleLeft")				
-	velocity = velocity.normalized() * speed
+		velocity = Vector2.ZERO
+		if direction == 1:
+			$AnimatedSprite.play("idleUp")
+		elif direction == 2:
+			$AnimatedSprite.play("idleDown")
+		elif direction == 3:
+			$AnimatedSprite.play("idleRight")
+		elif direction == 4:
+			$AnimatedSprite.play("idleLeft")
+
+	velocity = velocity.normalized() * speed * sprint_multiplier
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
