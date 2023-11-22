9e3babbed57080aec7a0ac4e77d4becfcf4fa7ae
ToastedFood <76954090+ToastedFood@users.noreply.github.com>
2023-11-22 14:03:01 -0500

    better sprinting

--- Player.gd
+++ Player.gd
@@ -4,6 +4,7 @@ var speed = 7000
 var velocity = Vector2.ZERO
 var direction = 1
 var frozen = false
+var is_sprinting = false
 
 func get_input():
 	var sprint_multiplier = 1.0
@@ -12,19 +13,10 @@ func get_input():
 	if Input.is_action_pressed("sprint"):
 		sprint_multiplier = 1.3
 		$AnimatedSprite.speed_scale = sprint_multiplier
-		if direction == 1:
-			velocity.y -= 1
-			$AnimatedSprite.play("walkUp")
-		if direction == 2:
-			velocity.y += 1
-			$AnimatedSprite.play("walkDown")
-		if direction == 3:
-			velocity.x += 1
-			$AnimatedSprite.play("sprintRight")
-		if direction == 4:
-			velocity.x -= 1
-			$AnimatedSprite.play("sprintLeft")
-	elif Input.is_action_pressed("topDownForward") or Input.is_action_pressed("ui_up"):
+		is_sprinting = true
+	else:
+		is_sprinting = false
+	if Input.is_action_pressed("topDownForward") or Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 		$AnimatedSprite.play("walkUp")
 		direction = 1
@@ -34,11 +26,17 @@ func get_input():
 		direction = 2
 	elif Input.is_action_pressed("topDownRight") or Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-		$AnimatedSprite.play("walkRight")
+		if not is_sprinting:
+			$AnimatedSprite.play("walkRight")
+		else:
+			$AnimatedSprite.play("sprintRight")
 		direction = 3
 	elif Input.is_action_pressed("topDownLeft") or Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-		$AnimatedSprite.play("walkLeft")
+		if is_sprinting:
+			$AnimatedSprite.play("sprintLeft")
+		else:
+			$AnimatedSprite.play("walkLeft")
 		direction = 4
 	else:
 		velocity = Vector2.ZERO
