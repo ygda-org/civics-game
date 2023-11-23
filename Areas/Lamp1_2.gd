16144cd6d7f417f5c11fa44ebb0b1f7e4d2833b0
stxrrymuffin <jessica.chen270412@gmail.com>
2023-11-23 01:54:04 -0500

    Revert "buttons and ui symbols"
    This reverts commit 2dc10b8ff812ae883a63b7088dd55eebd4161645.

--- Areas/Lamp1_2.gd
+++ Areas/Lamp1_2.gd
@@ -8,6 +8,7 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(get_node("Lamp Sprite").z_index)
 	pass # Replace with function body.
 
 
