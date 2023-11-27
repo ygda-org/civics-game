2fce2c80eb444f2400ebedec1d79d75d42319c2e
ToastedFood <76954090+ToastedFood@users.noreply.github.com>
2023-11-26 23:05:28 -0500

    fixed running on school problem

--- Areas/schoolTop.gd
+++ Areas/schoolTop.gd
@@ -18,15 +18,16 @@ func _ready():
 
 func _on_schoolTop_body_exited(body):
 	if "Player" in body.name:
+		body.behindCount -= 1
+		if(body.behindCount == 0):
+			body.z_index = 4
+
+
+func _on_schoolTop_body_entered(body):
+	if "Player" in body.name:
 		print("on;l")
 		body.behindCount += 1
 		if(body.z_index > z_index -1):
 			body.z_index = z_index -1 # Replace with function body.
 		#print(body.z_index)
 
-
-func _on_schoolTop_body_entered(body):
-	if "Player" in body.name:
-		body.behindCount -= 1
-		if(body.behindCount == 0):
-			body.z_index = 4
